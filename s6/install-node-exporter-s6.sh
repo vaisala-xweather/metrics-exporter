93570479bc6020eafb0e26c2b29a0822bd07298a
Chance Harrison <chance.harrison@vaisala.com>
2026-02-05 16:13:41 -0500

    Fixed syntax error in s6 install script and updated node-exporter s6 install script to use log rotation

--- s6/install-node-exporter-s6.sh
+++ s6/install-node-exporter-s6.sh
@@ -10,23 +10,51 @@ echo "Installing node-exporter..."
 
 NODE_EXPORTER_VERSION="${NODE_EXPORTER_VERSION:-1.6.0}"
 
-adduser -M -r -s "/sbin/nologin node_exporter"
+adduser -M -r -s /sbin/nologin node_exporter
 cd "/usr/src/"
 wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
 tar -xf "node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz"
 cd "node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64"
 cp "node_exporter" "/usr/bin/"
 
-mkdir -p "$S6_DIR/node-exporter"
-echo "longrun" > "$S6_DIR/node-exporter/type"
+# Setup logging for service
+mkdir -p $S6_DIR/node-exporter-log-prepare/dependencies.d
+touch $S6_DIR/node-exporter-log-prepare/dependencies.d/base
+
+echo "oneshot" > $S6_DIR/node-exporter-log-prepare/type
+cat << EOF > $S6_DIR/node-exporter-log-prepare/up
+if { mkdir -p /var/log/node-exporter }
+if { chown nobody:nobody /var/log/node-exporter }
+chmod 02755 /var/log/node-exporter
+EOF
+
+mkdir -p $S6_DIR/node-exporter-log/dependencies.d
+touch $S6_DIR/node-exporter-log/dependencies.d/node-exporter-log-prepare
+echo "longrun" > $S6_DIR/node-exporter-log/type
+echo "node-exporter" > $S6_DIR/node-exporter-log/consumer-for
+
+cat << EOF > $S6_DIR/node-exporter-log/run
+#!/bin/sh
+exec logutil-service /var/log/node-exporter
+EOF
+chmod 700 $S6_DIR/node-exporter-log/run
+
+echo "node-exporter-pipeline" > $S6_DIR/node-exporter-log/pipeline-name
+touch $S6_DIR/user/contents.d/node-exporter-pipeline
+
+
+# Setup service
+mkdir -p $S6_DIR/node-exporter/dependencies.d
+touch $S6_DIR/node-exporter/dependencies.d/base
+echo "longrun" > $S6_DIR/node-exporter/type
+echo "node-exporter-log" > $S6_DIR/node-exporter/producer-for
 
 cat << EOF > "$S6_DIR/node-exporter/run"
 #!/bin/sh
-/usr/sbin/runuser -u node_exporter -- /usr/bin/node_exporter --collector.textfile.directory=/tmp/node_exporter_collector --web.listen-address=:9101
+exec 2>&1
+exec /usr/sbin/runuser -u node_exporter -- /usr/bin/node_exporter --collector.textfile.directory=/tmp/node_exporter_collector --web.listen-address=:9101
 EOF
 chmod 700 "$S6_DIR/node-exporter/run"
-
-touch "$S6_DIR/user/contents.d/node-exporter"
 
 mkdir "/tmp/node_exporter_collector"
 chmod 777 "/tmp/node_exporter_collector"
